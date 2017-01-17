28a7f8cc3889750d1a24d415f8f0f00f83879643
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 12:50:58 +0200

    Basic inflate Navigator class(Make Singlote)

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/navigation/Navigator.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/navigation/Navigator.java
@@ -1,4 +1,19 @@
 package com.internship.pbt.bizarechat.presentation.navigation;
 
 public class Navigator {
+
+    private static Navigator mInstance;
+
+    public static Navigator getInstance(){
+
+        if (mInstance == null)
+            mInstance = new Navigator();
+
+        return mInstance;
+    }
+    private Navigator() {
+        super();
+    }
+
+    //TODO Navigate method`s
 }
