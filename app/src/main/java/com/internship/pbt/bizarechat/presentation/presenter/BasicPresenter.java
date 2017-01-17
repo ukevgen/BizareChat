8eff1d184f5f737f93a751eaa9130853931a0325
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 18:16:38 +0200

    Add Presenter  Interface, Delete BasicPresenter.class

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/presenter/BasicPresenter.java
+++
@@ -1,4 +0,0 @@
-package com.internship.pbt.bizarechat.presentation.presenter;
-
-public class BasicPresenter {
-}
---
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/presenter/Presenter.java
@@ -0,0 +1,11 @@
+package com.internship.pbt.bizarechat.presentation.presenter;
+
+public interface Presenter {
+
+    public void resume();
+
+    public void pause();
+
+    public void destroy();
+
+}
