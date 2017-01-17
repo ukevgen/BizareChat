6739cf7d52a6d3e3daac7b85634c647974296945
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 17:27:34 +0200

    Small after-merge fixes

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/BizareChatApp.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/BizareChatApp.java
@@ -8,6 +8,7 @@ import com.internship.pbt.bizarechat.BuildConfig;
 import io.fabric.sdk.android.Fabric;
 
 public class BizareChatApp extends Application {
+
     @Override public void onCreate() {
         super.onCreate();
         if(BuildConfig.CRASH_REPORTS)
