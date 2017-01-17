62bd4ae39b6af2b78c88ebe0c89f4bead442fd41
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 19:14:22 +0200

    Define UseCase.class

--- app/src/main/java/com/internship/pbt/bizarechat/domain/interactor/UseCase.java
+++ app/src/main/java/com/internship/pbt/bizarechat/domain/interactor/UseCase.java
@@ -1,6 +1,35 @@
 package com.internship.pbt.bizarechat.domain.interactor;
 
+import com.internship.pbt.bizarechat.domain.executor.PostExecutorThread;
+import com.internship.pbt.bizarechat.domain.executor.ThreadExecutor;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.subscriptions.Subscriptions;
+import rx.schedulers.Schedulers;
+
 public abstract class UseCase {
 
-    //TODO create UseCase`s
+    private ThreadExecutor mThreadExecutor;
+    private PostExecutorThread mPostExecutorThread;
+    private Subscription mSubscription = Subscriptions.empty();
+
+    protected UseCase(ThreadExecutor threadExecutor,
+                      PostExecutorThread postExecutionThread) {
+        this.mThreadExecutor = threadExecutor;
+        this.mPostExecutorThread = postExecutionThread;
+    }
+    protected abstract Observable buildUseCaseObservable();
+
+    @SuppressWarnings("unchecked")
+    public void execute(Subscriber useCaseSubscriber) {
+        this.mSubscription = this.buildUseCaseObservable()
+                .subscribeOn(Schedulers.from(mThreadExecutor))
+                .observeOn(mPostExecutorThread.getSheduler())
+                .subscribe(useCaseSubscriber);
+    }
+
+
+
 }
