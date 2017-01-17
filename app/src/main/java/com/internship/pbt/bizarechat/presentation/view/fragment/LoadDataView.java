2eeeb1814069d0d6e26a640d6baf8a2d21e7aa86
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 22:06:52 +0200

    Add LoadDataView interface

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/view/fragment/LoadDataView.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/view/fragment/LoadDataView.java
@@ -1,17 +1,15 @@
 package com.internship.pbt.bizarechat.presentation.view.fragment;
 
-import android.content.Context;
-
 public interface LoadDataView {
 
     void showLoading();
-    
+
     void hideLoading();
-    
+
     void showRetry();
-    
+
     void hideRetry();
-    
+
     void showError(String message);
-    
+
 }
