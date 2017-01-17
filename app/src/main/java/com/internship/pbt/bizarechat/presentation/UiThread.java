1b742157b4137ab0eadbcf506c876ad08373a1ba
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 18:39:01 +0200

    UiThread implement PostExecutorThread

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/UiThread.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/UiThread.java
@@ -3,10 +3,11 @@ package com.internship.pbt.bizarechat.presentation;
 import com.internship.pbt.bizarechat.domain.executor.PostExecutorThread;
 
 import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
 
 public class UiThread implements PostExecutorThread{
 
     @Override public Scheduler getSheduler() {
-        return null; // TODO AnroidShedulers.mainThread();
+        return AndroidSchedulers.mainThread();
     }
 }
