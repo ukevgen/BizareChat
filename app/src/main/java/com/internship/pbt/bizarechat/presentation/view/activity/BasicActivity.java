8a128372897665dd04575ccaf1123279ebb92334
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 12:52:24 +0200

    Instance Navigator in BasicActivity

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/view/activity/BasicActivity.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/view/activity/BasicActivity.java
@@ -1,7 +1,15 @@
 package com.internship.pbt.bizarechat.presentation.view.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.internship.pbt.bizarechat.presentation.navigation.Navigator;
 
 public class BasicActivity extends Activity {
 
+    Navigator mNavigator;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
