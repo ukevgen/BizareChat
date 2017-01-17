83132a4678473da41ef6845b15d654c9d45ceafa
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 13:37:42 +0200

    add instance of Navigator in BasicActivity -> onResume method

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/view/activity/BasicActivity.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/view/activity/BasicActivity.java
@@ -7,9 +7,16 @@ import com.internship.pbt.bizarechat.presentation.navigation.Navigator;
 
 public class BasicActivity extends Activity {
 
-    Navigator mNavigator;
+    protected Navigator mNavigator;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+
+        if (mNavigator == null)
+            mNavigator = Navigator.getInstance();
     }
 }
