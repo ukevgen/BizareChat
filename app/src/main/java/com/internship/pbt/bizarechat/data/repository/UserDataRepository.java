a11a4a2e458a237e1b743d0190ffdd131b5b84d2
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 18:40:31 +0200

    Small import fix in UserDataRepository

--- app/src/main/java/com/internship/pbt/bizarechat/data/repository/UserDataRepository.java
+++ app/src/main/java/com/internship/pbt/bizarechat/data/repository/UserDataRepository.java
@@ -1,7 +1,7 @@
 package com.internship.pbt.bizarechat.data.repository;
 
-import com.pack.agilieintershiptestproject.domain.scr.main.repository.UserRepository;
 
+import com.internship.pbt.bizarechat.domain.repository.UserRepository;
 
 public class UserDataRepository implements UserRepository {
 }
