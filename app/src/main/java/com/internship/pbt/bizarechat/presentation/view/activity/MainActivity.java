7b08f09e6756814266bcd7980055fea3b2edcbc6
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-17 14:14:20 +0200

    Create MainActivity.class and activity_layout.xml with fragment container

---
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/view/activity/MainActivity.java
@@ -0,0 +1,10 @@
+package com.internship.pbt.bizarechat.presentation.view.activity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BasicActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
