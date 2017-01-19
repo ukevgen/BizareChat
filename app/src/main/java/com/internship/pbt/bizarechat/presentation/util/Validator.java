71c5336d23ae030854c207375122e88d94262b19
Roman Kapshuk <kapshuk96@gmail.com>
2017-01-20 00:09:59 +0200

    Add isValidPassword method in validator

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/util/Validator.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/util/Validator.java
@@ -3,9 +3,13 @@ package com.internship.pbt.bizarechat.presentation.util;
 
 import android.util.Patterns;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
 
-    private final String PASSWORD_REGEX = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}).{1,}";
+    private final String PASSWORD_REGEX = "((?=.*\\d).{2,})((?=.*[a-z]))((?=.*[A-Z]).{2,})";
+
     public boolean isValidEmail(String email){
         return Patterns.EMAIL_ADDRESS.matcher(email.trim()).matches();
     }
@@ -15,6 +19,8 @@ public class Validator {
     }
 
     public boolean isValidPassword(String password){
-        return false; //TODO password validator
+        Pattern patterns = Pattern.compile(PASSWORD_REGEX);
+        Matcher matcher = patterns.matcher(password);
+        return matcher.matches();
     }
 }
