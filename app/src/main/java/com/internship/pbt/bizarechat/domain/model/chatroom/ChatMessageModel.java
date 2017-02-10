43a7853570483059ceda3af25e1a8621314836cc
Andre <dron1123581321@gmail.con>
2017-02-10 14:20:02 +0200

    Chat room ui was implemented (including 9-patch images) Adapter was created for RecyclerView with messages.

---
+++ app/src/main/java/com/internship/pbt/bizarechat/domain/model/chatroom/ChatMessageModel.java
@@ -0,0 +1,51 @@
+package com.internship.pbt.bizarechat.domain.model.chatroom;
+
+
+
+public class ChatMessageModel {
+    private String text;
+    private String userName;
+    private long userId;
+    private String time;
+    private int state;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
