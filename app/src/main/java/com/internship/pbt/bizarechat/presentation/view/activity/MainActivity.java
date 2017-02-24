76407ff1151754d6c047a22b340acbd4b77fe61c
Andre <dron1123581321@gmail.con>
2017-02-24 23:41:38 +0200

    User info screen implemented: 1. UI 2. Animated transitions between user info and users screens 3. Dialing number (if cellular service is available) 4. Sending email 5. Opening website

--- app/src/main/java/com/internship/pbt/bizarechat/presentation/view/activity/MainActivity.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/view/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -27,6 +28,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -87,6 +89,7 @@ public class MainActivity extends MvpAppCompatActivity implements
     private Navigator navigator = Navigator.getInstance();
     private DrawerLayout mDrawer;
     private Toolbar mToolbar;
+    private CollapsingToolbarLayout toolbarLayout;
     private AppBarLayout.LayoutParams toolbarParams;
     private TabLayout mTabLayout;
     private FloatingActionButton fab;
@@ -94,6 +97,7 @@ public class MainActivity extends MvpAppCompatActivity implements
     private BizareChatMessageService messageService;
     private ServiceConnection messageServiceConnection;
     private Intent messageServiceIntent;
+    private ProgressBar progressBar;
 
     public static Intent getCallingIntent(Context context) {
         return new Intent(context, MainActivity.class);
@@ -161,10 +165,14 @@ public class MainActivity extends MvpAppCompatActivity implements
         mTabLayout.addTab(mTabLayout.newTab().setText("Private"));
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbarParams = (AppBarLayout.LayoutParams) mToolbar.getLayoutParams();
+        toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
+        toolbarParams = (AppBarLayout.LayoutParams) toolbarLayout.getLayoutParams();
         setSupportActionBar(mToolbar);
         mTextOnToolbar = (TextView) findViewById(R.id.chat_toolbar_title);
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        progressBar = (ProgressBar)findViewById(R.id.main_progress_bar);
+        progressBar.getIndeterminateDrawable()
+                .setColorFilter(getResources().getColor(R.color.colorPrimary), android.graphics.PorterDuff.Mode.MULTIPLY);
     }
 
     @Override
@@ -230,7 +238,7 @@ public class MainActivity extends MvpAppCompatActivity implements
                 break;
             case R.id.invite_friends:
                 presenter.inviteFriends();
-
+                break;
         }
     }
 
--- app/src/main/java/com/internship/pbt/bizarechat/presentation/view/fragment/users/UsersView.java
+++ app/src/main/java/com/internship/pbt/bizarechat/presentation/view/fragment/users/UsersView.java
@@ -2,6 +2,9 @@ package com.internship.pbt.bizarechat.presentation.view.fragment.users;
 
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
+import com.internship.pbt.bizarechat.data.datamodel.UserModel;
 
 public interface UsersView extends MvpView{
     void showLoading();
@@ -11,4 +14,7 @@ public interface UsersView extends MvpView{
     void showNetworkError();
 
     void showAloneMessage();
+
+    @StateStrategyType(SkipStrategy.class)
+    void showUserInfo(UserModel user);
 }
